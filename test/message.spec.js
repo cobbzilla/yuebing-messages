7594f843910db0ec408c1853e46ae021824b7e73
Jonathan Cobb <jonathan@kyuss.org>
2023-08-23 23:38:33 -0400

    add capitalize/uncapitalize utility functions, fix test

--- test/message.spec.js
+++ test/message.spec.js
@@ -2,21 +2,22 @@ import { describe, it } from "mocha";
 import { expect } from "chai";
 
 import * as msg from "../lib/esm/index.js";
+import { fieldErrorMessage, parseMessage } from "hokey-runtime";
 
 describe("template message test", async () => {
     it("properly renders a message with a templated value", async () => {
         const m = msg.MESSAGES["en"];
-        const rendered = msg.parseMessage("error_field_required", m, { field: "TEST" });
+        const rendered = parseMessage("error_field_required", m, { field: "TEST" });
         expect(rendered).eq("TEST is required");
     });
     it("properly renders a message with a missing template value", async () => {
         const m = msg.MESSAGES["en"];
-        const rendered = msg.parseMessage("error_field_required", m, {});
+        const rendered = parseMessage("error_field_required", m, {});
         expect(rendered).eq("?!field is required");
     });
     it("fieldErrorMessage properly renders an error message", async () => {
         const m = msg.MESSAGES["en"];
-        const err = msg.fieldErrorMessage("usernameOrEmail", "required", m);
+        const err = fieldErrorMessage("usernameOrEmail", "required", m);
         expect(err).eq("Username or email is required");
     });
 });
